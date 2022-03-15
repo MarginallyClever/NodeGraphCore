8eb9238e2f895875fda83c0c59ce12b7355e51c0
Dan Royer <dan@marginallyclever.com>
2022-03-15 12:54:48 -0700

    Create FileHelper.java

---
+++ src/main/java/com/marginallyClever/nodeGraphCore/FileHelper.java
@@ -0,0 +1,33 @@
+package com.marginallyClever.nodeGraphCore;
+
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Convenient methods for searching files in a directory and finding the user's donatello/extensions path.
+ */
+public class FileHelper {
+    /**
+     * Returns The contents of a path.
+     * @param dir the path to enumerate.
+     * @return The contents of a path.
+     */
+    public static Set<String> listFilesInDirectory(String dir) {
+        return Stream.of(new File(dir).listFiles())
+                .filter(file -> !file.isDirectory())
+                .map(File::getName)
+                .collect(Collectors.toSet());
+    }
+
+    /**
+     * Returns the extension path ~/Donatello/extensions/
+     * @return the extension path ~/Donatello/extensions/
+     */
+    public static String getExtensionPath() {
+        String sep = FileSystems.getDefault().getSeparator();
+        return System.getProperty("user.home") + sep + "Donatello" + sep +"extensions";
+    }
+}
