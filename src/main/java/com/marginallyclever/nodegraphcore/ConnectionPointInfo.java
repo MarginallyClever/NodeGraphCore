a6b8be2336d4c2cb943e6e8f3f99b294c936d608
Dan Royer <dan@marginallyclever.com>
2023-09-27 13:54:07 -0700

    secure interface

--- src/main/java/com/marginallyclever/nodegraphcore/ConnectionPointInfo.java
+++ src/main/java/com/marginallyclever/nodegraphcore/ConnectionPointInfo.java
@@ -10,16 +10,6 @@ import java.awt.*;
  */
 public class ConnectionPointInfo {
     /**
-     * The {@link Node} containing the {@link Dock} with a connection point.
-     */
-    public Node node;
-
-    /**
-     * The index of the {@link Dock} within this node with the connection point.
-     */
-    public int dockIndex;
-
-    /**
      * Indicates this connection point is an output from a {@link Node}.
      */
     public static final int OUT=2;
@@ -29,23 +19,31 @@ public class ConnectionPointInfo {
      */
     public static final int IN=1;
 
-    /**
-     * {@link ConnectionPointInfo#IN} or {@link ConnectionPointInfo#OUT}.
-     */
-    public int flags;
+    private Node node;
+
+    private int dockIndex;
+
+    private int flags;
 
     /**
      * Constructor for subclasses to call.
      */
-    public ConnectionPointInfo() {}
+    protected ConnectionPointInfo() {}
 
     /**
      * Constructor for subclasses to call.
-     * @param node a {@link Node}
+     * @param node a {@link Node}.
      * @param dockIndex the index to a variable inside the node.
      * @param flags {@link ConnectionPointInfo#IN} or {@link ConnectionPointInfo#OUT}
+     * @throws IllegalArgumentException if the given node is null.
+     * @throws IllegalArgumentException if the given dockIndex is less than 0.
+     * @throws IllegalArgumentException if the given flags is not {@link ConnectionPointInfo#IN} or {@link ConnectionPointInfo#OUT}
      */
     public ConnectionPointInfo(Node node, int dockIndex, int flags) {
+        if(node==null) throw new IllegalArgumentException("node cannot be null");
+        if(dockIndex<0) throw new IllegalArgumentException("dockIndex cannot be negative");
+        if(flags!=IN && flags!=OUT) throw new IllegalArgumentException("flags must be IN or OUT");
+
         this.node=node;
         this.dockIndex = dockIndex;
         this.flags=flags;
@@ -56,11 +54,32 @@ public class ConnectionPointInfo {
      * @return the variable
      */
     public Dock<?> getVariable() {
-        return node.getVariable(dockIndex);
+        return getNode().getVariable(getDockIndex());
     }
 
     public Point getPoint() {
         Dock<?> v = getVariable();
-        return (flags == IN) ? v.getInPosition() : v.getOutPosition();
+        return (getFlags() == IN) ? v.getInPosition() : v.getOutPosition();
+    }
+
+    /**
+     * The index of the {@link Dock} within this node with the connection point.
+     */
+    public int getDockIndex() {
+        return dockIndex;
+    }
+
+    /**
+     * The {@link Node} containing the {@link Dock} with a connection point.
+     */
+    public Node getNode() {
+        return node;
+    }
+
+    /**
+     * {@link ConnectionPointInfo#IN} or {@link ConnectionPointInfo#OUT}.
+     */
+    public int getFlags() {
+        return flags;
     }
 }
