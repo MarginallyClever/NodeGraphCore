99a3900e7009cdbd00ef5f7aa2b6bd0b0fa0415c
Dan Royer <dan@marginallyclever.com>
2023-09-27 13:52:27 -0700

    document

--- src/main/java/com/marginallyclever/nodegraphcore/DAORegistry.java
+++ src/main/java/com/marginallyclever/nodegraphcore/DAORegistry.java
@@ -3,15 +3,17 @@ package com.marginallyclever.nodegraphcore;
 /**
  * NodeRegistry is a Service that can load {@link DAO4JSON}s into the local {@link DAO4JSONFactory}.
  * It should be declared in the <b>module-info.java</b> as
- * <pre>
- * provides com.marginallyclever.nodegraphcore.DAORegistry
+ * <pre>provides com.marginallyclever.nodegraphcore.DAORegistry
  *          with your.implementing.class;</pre>
- * See also <a href='https://docs.oracle.com/javase/9/docs/api/java/util/ServiceLoader.html'>Deploying service providers as modules</a>
+ * See also <a href="https://docs.oracle.com/javase/9/docs/api/java/util/ServiceLoader.html">Deploying service providers as modules</a>
  * @author Dan Royer
  * @since 2022-03-12
  */
 public interface DAORegistry {
     String getName();
 
+    /**
+     * Register DAOs.
+     */
     void registerDAO();
 }
