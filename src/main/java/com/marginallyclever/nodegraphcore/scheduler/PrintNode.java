6347cd2a87189e171798ff874798d9ea1a98d730
Dan Royer <dan@marginallyclever.com>
2025-01-14 11:30:31 -0800

    remove dead nodes from simple test case

--- src/main/java/com/marginallyclever/nodegraphcore/scheduler/PrintNode.java
+++
@@ -1,9 +0,0 @@
-package com.marginallyclever.nodegraphcore.scheduler;
-
-public class PrintNode extends Node {
-    @Override
-    protected Object process(Object input) {
-        System.out.println("Output: " + input);
-        return null;
-    }
-}
