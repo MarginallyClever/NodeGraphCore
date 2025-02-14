4f09678e4bd40c3c254f8dcf1a94086923418258
Dan Royer <dan@marginallyclever.com>
2025-02-14 13:13:53 -0800

    Update GraphException.java

--- src/main/java/com/marginallyclever/nodegraphcore/GraphException.java
+++ src/main/java/com/marginallyclever/nodegraphcore/GraphException.java
@@ -1,8 +1,6 @@
 package com.marginallyclever.nodegraphcore;
 
-public class GraphException extends RuntimeException{
-    private static final long serialVersionUID = 1L;
-
+public class GraphException extends RuntimeException {
     public GraphException(String message) {
         super(message);
     }
